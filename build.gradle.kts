392b24801ae49dc34dcc2ce423ccafa6b81c3429
Titrom25 <titrom25@mail.ru>
2022-11-18 23:09:27 +0700

    Add jacoco test coverage + sonarcloud integration

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,15 +4,37 @@ plugins {
     kotlin("jvm") version "1.7.20"
     kotlin("plugin.serialization") version "1.7.20"
     id("org.sonarqube") version "3.5.0.2730"
+    jacoco
     application
+}
+
+tasks.test {
+    useJUnitPlatform()
+    finalizedBy(tasks.jacocoTestReport)
+}
+
+jacoco {
+    toolVersion = "0.8.8"
+    reportsDirectory.set(layout.projectDirectory.dir("build/jacocoReport"))
+}
+
+tasks.jacocoTestReport {
+    dependsOn(tasks.test)
+    reports {
+        xml.required.set(true)
+    }
 }
 
 sonarqube {
     properties {
-        property("sonar.sourceEncoding", "UTF-8")
+        property("sonar.projectKey", "Titrom025_ldap-users-service-prototype")
+        property("sonar.organization", "titrom025")
         property("sonar.host.url", "https://sonarcloud.io")
-        property("sonar.organization", "tmp-production")
-        property("sonar.projectKey", "tmp-production_ldap-users-service-prototype")
+        property("sonar.scm.disabled", "true")
+        property(
+            "sonar.coverage.jacoco.xmlReportPaths",
+            "build/jacocoReport/test/jacocoTestReport.xml"
+        )
     }
 }
 
@@ -24,11 +46,6 @@ repositories {
 }
 
 dependencies {
-    testImplementation(kotlin("test"))
-
-//    implementation("org.apache.logging.log4j:log4j-api:2.19.0")
-//    implementation("org.apache.logging.log4j:log4j-core:2.19.0")
-
     implementation("org.slf4j:slf4j-api:2.0.3")
     implementation("org.slf4j:slf4j-simple:2.0.3")
 
@@ -37,22 +54,14 @@ dependencies {
 
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.1")
     implementation("io.ktor:ktor-server-content-negotiation:2.1.3")
-//    implementation("io.ktor:ktor-gson:2.1.3")
-//    implementation("io.ktor:ktor-gson:1.6.8")
-    implementation("io.ktor:ktor-serialization-kotlinx-json:2.1.3")
-//    implementation("io.ktor:ktor-serialization-kotlinx-xml:2.1.3")
 
-    // ktor-client for making requests
+    implementation("io.ktor:ktor-serialization-kotlinx-json:2.1.3")
+
     implementation("io.ktor:ktor-client-core:2.1.3")
     implementation("io.ktor:ktor-client-cio:2.1.3")
     implementation("io.ktor:ktor-client-logging:2.1.3")
 
-    testImplementation("io.ktor:ktor-server-tests-jvm:2.1.3")
-//    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:1.7.20")
-}
-
-tasks.test {
-    useJUnitPlatform()
+    testImplementation(kotlin("test"))
 }
 
 tasks.withType<KotlinCompile> {

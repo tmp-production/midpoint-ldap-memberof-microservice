4a2453a3dd8c4c7a538edfaa862fe9ecd2b35a23
Roman <b111forjob@gmail.com>
2022-12-10 12:21:30 +0700

    Add unit tests

--- src/test/kotlin/com/tmpproduction/ldapservice/MemberOfServiceTest.kt
+++ src/test/kotlin/com/tmpproduction/ldapservice/MemberOfServiceTest.kt
@@ -1,13 +1,48 @@
 package com.tmpproduction.ldapservice
 
+import com.tmpproduction.ldapservice.stubs.SequenceProviderService
+import com.tmpproduction.ldapservice.stubs.SimpleRepo
+import com.tmpproduction.ldapservice.stubs.UnavailableRepo
+import kotlinx.coroutines.runBlocking
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 
 internal class MemberOfServiceTest {
 
     @Test
-    fun testFoo() {
+    fun serviceIsUnavailable() {
+        val providerService = SequenceProviderService {
+            runBlocking {
+                assertFalse(isAvailable())
+            }
+        }
+        val memberOfService = MemberOfService(
+            requestsProviderService = providerService,
+            midpointStateProvider = UnavailableRepo()
+        )
+        memberOfService.start()
+    }
 
+    @Test
+    fun simpleUseCase() {
+        val userName = "bill gates"
+        val newMemberOf = "new member of"
+        val providerService = SequenceProviderService {
+            runBlocking {
+                assertTrue(isAvailable())
+                val userOid = getUserByFullName(userName)!!
+                val shadows = getUserShadows(userOid)
+                assertTrue(shadows.isNotEmpty())
+                val ldapShadow = shadows.filter { shadowBelongsToLDAP(it) }
+                assertTrue(ldapShadow.size == 1)
+                setMemberOf(ldapShadow[0], newMemberOf)
+            }
+        }
+        val memberOfService = MemberOfService(
+            requestsProviderService = providerService,
+            midpointStateProvider = SimpleRepo(userName)
+        )
+        memberOfService.start()
     }
 
 }
---
+++ src/test/kotlin/com/tmpproduction/ldapservice/stubs/Stubs.kt
@@ -0,0 +1,76 @@
+package com.tmpproduction.ldapservice.stubs
+
+import com.tmpproduction.ldapservice.MidpointRepository
+import com.tmpproduction.ldapservice.OID
+import com.tmpproduction.ldapservice.RequestsProviderService
+
+class SequenceProviderService(
+    private inline val task: MidpointRepository.() -> Unit
+) : RequestsProviderService {
+    override fun start(target: MidpointRepository) = task(target)
+}
+
+open class UnavailableRepo : MidpointRepository {
+    override suspend fun isAvailable(): Boolean {
+        return false
+    }
+
+    override suspend fun getUserByFullName(fullName: String): OID? {
+        throw NotImplementedError()
+    }
+
+    override suspend fun getUserShadows(user: OID): List<OID> {
+        throw NotImplementedError()
+    }
+
+    override suspend fun shadowBelongsToLDAP(shadow: OID): Boolean {
+        throw NotImplementedError()
+    }
+
+    override suspend fun setMemberOf(shadow: OID, newMemberOf: String) {
+        throw NotImplementedError()
+    }
+
+}
+
+/**
+ * Contains a user under fullname "target user"
+ */
+class SimpleRepo(
+    private val userFullName: String
+) : MidpointRepository {
+
+    private val userOID: OID = "0000-0000-0000-0021"
+    private val userLdapShadow: OID = "0000-0000-0020-0021"
+    private val userShadows  = listOf(
+        "0000-0000-0001-0021",
+        userLdapShadow
+    )
+
+    override suspend fun isAvailable(): Boolean = true
+
+    override suspend fun getUserByFullName(fullName: String): OID? {
+        if (fullName == userFullName) {
+            // Todo(Roman B.) check it is a valid OID just in case
+            return userOID
+        }
+        return null
+    }
+
+    override suspend fun getUserShadows(user: OID): List<OID> {
+        if (user == userOID) {
+            return userShadows
+        }
+        return emptyList()
+    }
+
+    override suspend fun shadowBelongsToLDAP(shadow: OID): Boolean = shadow == userLdapShadow
+
+
+    override suspend fun setMemberOf(shadow: OID, newMemberOf: String) {
+        if (shadow != userLdapShadow) {
+            throw IllegalArgumentException()
+        }
+    }
+
+}
